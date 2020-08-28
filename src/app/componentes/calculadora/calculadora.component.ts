61cf38f70173e7e1a59d1a4c02e30106613dcbdd
Santiago <Santiagodiazboutet@gmail.com>
2020-08-27 22:35:05 -0300

    Ejercicio 1 completo

--- src/app/componentes/calculadora/calculadora.component.ts
+++ src/app/componentes/calculadora/calculadora.component.ts
@@ -9,6 +9,7 @@ export class CalculadoraComponent implements OnInit {
   numero1=0;
   numero2=0;
   promedio=0;
+  suma=0;
   constructor() { }
 
   ngOnInit(): void {
@@ -16,8 +17,10 @@ export class CalculadoraComponent implements OnInit {
 
   calcularPromedio(){
     this.promedio=(this.numero1+this.numero2)/2;
+    this.suma=this.numero1+this.numero2;
   }
   limpiarCuadro(){
     this.promedio=0;
+    this.suma=0;
   }
 }
